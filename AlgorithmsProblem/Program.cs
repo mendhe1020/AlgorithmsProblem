c86349614c1a4903124b3910730e0c31591e8e74
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-25 17:08:51 +0530

    [Anurag] Add: Algorithms Programs

---
+++ AlgorithmsProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmsProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithms Programs");
+        }
+    }
+}
