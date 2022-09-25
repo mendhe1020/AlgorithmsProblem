2a7cb7298a6bb44eda1a60d4774362dc7716611b
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-25 21:19:30 +0530

    [Anurag] Add: Insertion Sort

--- AlgorithmsProblem/Program.cs
+++ AlgorithmsProblem/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace AlgorithmsProblem
+﻿using AlgorithmsProblem;
+
+namespace AlgorithmsProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            BinarySearch binarySearch = new BinarySearch();
-            binarySearch.Search(@"C:\Users\anura\Music\AlgorithmsProblem\AlgorithmsProblem\AlgorithmsProblem\Words.txt");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs \n1.Binary search \n2.Insertion sort\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch binarySearch = new BinarySearch();
+                        binarySearch.Search(@"C:\Users\anura\Music\AlgorithmsProblem\AlgorithmsProblem\AlgorithmsProblem\Words.txt");
+                        break;
+                    case 2:
+                        InsertionSort insertionsort = new InsertionSort();
+                        insertionsort.Insertion();
+                        break;
+                }
+            }
         }
     }
 }
